92b2f3f163b2ff79d72644fc69c8a140af2e631f
loganbarnhart01 <logan.barnhart.01@gmail.com>
2024-07-24 00:36:36 -0600

    modifications to wrapper for testing

--- models/reward_model.py
+++ models/reward_model.py
@@ -2,17 +2,17 @@ from torch import nn
 from transformers import AutoModelForSequenceClassification
 
 class RewardModelWrapper(nn.Module):
-    def __init__(self, model_name):
+    def __init__(self, model_name, **kwargs):
         super().__init__()
-        self.model = AutoModelForSequenceClassification.from_pretrained(model_name, trust_remote_code=True)
+        self.model = AutoModelForSequenceClassification.from_pretrained(model_name, trust_remote_code=True, **kwargs)
         self.base_model_prefix = self.model.base_model_prefix
     
     def forward(self, *args, **kwargs):
         output= self.model(*args, **kwargs)
-        if hasattr(output, 'hidden_state'):
-            # output.hidden_states = output.hidden_state
-            output.hidden_states = [output.score]
+        # if hasattr(output, 'hidden_state'):
+        #     # output.hidden_states = output.hidden_state
+        #     output.hidden_states = [output.score]
         return output
     
-    def score(self, hidden_states):
-        return hidden_states[0]
+    # def score(self, hidden_states):
+    #     return hidden_states[0]
