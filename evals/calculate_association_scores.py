a6605e97d9241e15d09d91fcf3ee99765168ae43
loganbarnhart01 <logan.barnhart.01@gmail.com>
2024-08-29 21:37:35 -0600

    quick way to see avg association scores

--- evals/calculate_association_scores.py
+++ evals/calculate_association_scores.py
@@ -100,11 +100,16 @@ def main(args):
         pass
         # Todo, save to scp to make nice plots of rankings
 
-    if task == "conviction":
-        pass
+    if task == "criminality":
+        print(model_scores)
+        for prompt in model_scores:
+            scores = model_scores[prompt]
+            model_scores[prompt] = [t.item() for t in scores]
+        scores_mean = mean_scores(model_scores)
+        print(scores_mean)
 
-    if task == "life_death":
-        pass
+    if task == "mean":
+        print(mean_scores(model_scores))
 
 def average_precision(traits, traits_true):
     def precision(traits_pred, traits_true):
